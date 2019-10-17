f9fea887142d5061c395a2ae2fa44e5338fe7138
Michel Santos Stefani <michel_estagiario@FCBM-318821.NFS_SAMBA>
2019-10-17 11:31:28 -0300

    Instalação e importação do mongoose e do model Product

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi",
+    {useUnifiedTopology: true, useNewUrlParser: true }
+);
+require('./src/models/Product');
+
+//Primeira rota
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World!!')
 });
 
 app.listen(3001);
